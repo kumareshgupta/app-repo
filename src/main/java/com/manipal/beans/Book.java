acf00c73360179820e6c94570074f367fb42598f
Kumaresh Gupta <kumaresh_gupta@yahoo.com>
2019-09-16 15:33:50 +0530

    first commit

---
+++ src/main/java/com/manipal/beans/Book.java
@@ -0,0 +1,39 @@
+package com.manipal.beans;
+
+public class Book {
+	private int bookId;
+	private String title;
+	private double price;
+	private String author;
+	
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	@Override
+	public String toString() {
+		return "Book [bookId=" + bookId + ", title=" + title + ", price=" + price + ", author=" + author + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/manipal/mains/Main.java
@@ -0,0 +1,20 @@
+package com.manipal.mains;
+
+import com.manipal.beans.Book;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GREAT");
+		
+		Book book = new Book();
+		book.setBookId(1001);
+		book.setTitle("Learn Microservice in 24 Hours");
+		book.setAuthor("Roza");
+		book.setPrice(399.00);
+		
+		System.out.println("Book Details : " + book);
+	}
+
+}
