949d14ce8bbabae17df996cd44b581bf6ce38c08
Kumaresh Gupta <kumaresh_gupta@yahoo.com>
2019-09-25 17:06:27 +0530

    Update Book.java

--- src/main/java/com/manipal/beans/Book.java
+++ src/main/java/com/manipal/beans/Book.java
@@ -39,7 +39,7 @@ public class Book {
 	}
 	@Override
 	public String toString() {
-		return "Book [bookId=" + bookId + ", title=" + title + ", price=" + price + ", author=" + author + "]";
+		return "Book Details-> Book Id : " + bookId + ", title=" + title + ", price=" + price + ", author=" + author;
 	}
 	
 	
