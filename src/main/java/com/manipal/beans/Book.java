6258259389a61cf023d9799ac1dc09a3c834b976
Kumaresh Gupta <kumaresh_gupta@yahoo.com>
2020-09-23 15:42:40 +0530

    Error corrected in Book.java

--- src/main/java/com/manipal/beans/Book.java
+++ src/main/java/com/manipal/beans/Book.java
@@ -39,7 +39,7 @@ public class Book {
 	}
 
 	public void show(){
-		System.out.println('Showing Book Details " + title);
+		System.out.println(" Showing Book Details " + title);
 	}
 	@Override
 	public String toString() {
